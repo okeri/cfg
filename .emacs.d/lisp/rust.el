0bb81b09fd3b0d62f03f96a5737a88364b244786
Oleg Keri <ezhi99@gmail.com>
2016-11-14 17:53:35 +0300

    added cargo config and rust

---
+++ .emacs.d/lisp/rust.el
@@ -0,0 +1,14 @@
+(require 'company-racer)
+(require 'rust-mode)
+(require 'racer)
+
+(setq racer-rust-src-path "/usr/src/rust/src")
+
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+(add-hook 'rust-mode-hook
+	  (lambda()
+	    (local-set-key [f7] 'cargo-process-build)
+	    (local-set-key [?\C-x ?\C-r] 'racer-describe)
+	    (local-set-key [?\C-x ?\C-d] 'racer-find-definition)
+	    (local-set-key [?\C-x ?d] 'racer--find-file)
+	    (racer-mode)))
