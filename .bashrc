c70409eafd56bce92a0527d73fb56b548049c6f8
Oleg Keri <ezhi99@gmail.com>
2018-03-17 21:49:07 +0300

    updated fish config

---
+++ .bashrc
@@ -0,0 +1,9 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+PS1='\W \e[0;36m❯ \e[0m'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,30 +3,39 @@ set -x XDG_RUNTIME_DIR /run/user/17
 set -x EDITOR "emacsclient -t"
 set -x LANG en_US.UTF-8
 set -x SWAYGRAB_FFMPEG_OPTS_DECODER '-c:v h264_nvenc -preset slow'
+set -x LEELOO_MAIN_QML /home/okeri/proj/pkg-leeloo/modules/ui/source/qml/Main.qml
+set -x CUDA_DIR /opt/cuda
+set -x LEELOO_TESTDATA /home/okeri/proj/pkg-leeloo/testdata
+set -x ARCH_AARCH64_COMPONENTS_ROOT /opt/aarch64-cross
 
 if not contains /opt/android-ndk $PATH
 	set -x PATH $PATH /opt/bin /opt/cuda/bin $ANDROID_SDK_ROOT/tools $ANDROID_SDK_ROOT/platform-tools $ANDROID_SDK_ROOT/build-tools/25.0.1/ /opt/android-ndk
 end
 
-
 function fish_right_prompt
 	set cmd_status_ $status
-	if test $cmd_status_  = 0
-		set cmd_status_ ''
-	else
-		echo -n (set_color red)
-		set cmd_status_ "[$cmd_status_]"
+	if test $cmd_status_ -ne 0
+		echo -n (set_color red) "[$cmd_status_] "
 	end
-	echo -n -s (set_color cyan)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)
+	echo -n -s (set_color cyan)(git rev-parse --abbrev-ref HEAD ^ /dev/null)(set_color normal)
+end
+
+function mc -d "Midnight Commander"
+	set old_shell $SHELL
+	set SHELL /bin/bash
+	command /bin/mc
+	set SHELL $old_shell
+end
+
+function cd
+	builtin cd $argv
+	ls
 end
 
 alias em 'emacsclient -t'
+alias emacs 'emacsclient -t'
 alias su 'su -'
-alias ll 'ls -la'
 
-if [ ! "$DISPLAY" ]
-	set -x XKB_DEFAULT_LAYOUT us
-	set -x XKB_DEFAULT_MODEL pc98
-	set -x XKB_DEFAULT_OPTIONS grp:toggle
-	sway
+if [ ! "$WAYLAND_DISPLAY" ]
+	env XKB_DEFAULT_LAYOUT=us XKB_DEFAULT_MODEL=pc98 XKB_DEFAULT_OPTIONS=grp:toggle sway
 end
